c86c8b5b5ad8e71440c9e090b0173d1ef935262e
Ana Caroline <140763719+aanaca@users.noreply.github.com>
2024-08-01 20:20:19 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -30,16 +30,15 @@ class Role(db.Model):
     def __repr__(self):
         return '<Role %r>' % self.name
 
+
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-    role = db.relationship('Role', backref='users')
 
     def __repr__(self):
         return '<User %r>' % self.username
-
 
 
 class NameForm(FlaskForm):
