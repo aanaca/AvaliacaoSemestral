5758ba1aafc3466d4fa3c4dd696e544ad775d7b4
Ana Caroline <140763719+aanaca@users.noreply.github.com>
2024-08-01 22:02:24 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, session, redirect, url_for, request
+import os
+from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
@@ -7,12 +8,12 @@ from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-# Configuração do app e inicialização das extensões
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 bootstrap = Bootstrap(app)
@@ -20,7 +21,6 @@ moment = Moment(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-# Modelos
 class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
@@ -36,11 +36,6 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-    role = db.relationship('Role', backref='users')
-
-    @property
-    def role_name(self):
-        return self.role.name if self.role else 'User'
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -49,6 +44,7 @@ class User(db.Model):
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
 
 
 @app.shell_context_processor
@@ -65,12 +61,11 @@ def page_not_found(e):
 def internal_server_error(e):
     return render_template('500.html'), 500
 
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        role_name = request.form.get('role', 'User')  # Obtém o cargo do formulário ou usa 'User' como padrão
+        role_name = request.form.get('role', 'user')  # Obtém o cargo do formulário ou usa 'user' como padrão
         role = Role.query.filter_by(name=role_name).first()
         if role is None:
             role = Role(name=role_name)
@@ -93,7 +88,3 @@ def index():
     
     return render_template('index.html', form=form, name=session.get('name'),
                            known=session.get('known', False), users=users)
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
