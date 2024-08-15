f55dbde9f03c54ddd8ab6fb23c63c12ef78b5036
Ana Caroline <140763719+aanaca@users.noreply.github.com>
2024-08-15 19:13:12 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,8 +3,7 @@ from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, SelectField
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SelectField, SubmitField
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -45,7 +44,7 @@ class User(db.Model):
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
-    role = SelectField('Role', choices=[('user', 'User'), ('mod', 'Moderator'), ('admin', 'Administrator')])
+    role = SelectField(u'Role?:', choices=[('Administrator'), ('Moderator'), ('User')])
     submit = SubmitField('Submit')
 
 
@@ -67,41 +66,21 @@ def internal_server_error(e):
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
-
+    user_all = User.query.all();
+    role_all = Role.query.all();
+    print(user_all);
     if form.validate_on_submit():
-        user = User.query.filter_by(username=form.name.data).first()
-
+        user = User.query.filter_by(username=form.name.data).first()                        
         if user is None:
-            # Obter a role baseada na escolha do formulário
-            role_name = form.role.data
-            user_role = Role.query.filter_by(name=role_name.capitalize()).first()
-
-            # Se a role não existir no banco de dados, você pode criar uma nova
-            if user_role is None:
-                user_role = Role(name=role_name.capitalize())
-                db.session.add(user_role)
-                db.session.commit()
-
-            # Criação do novo usuário com a role correta
-            user = User(username=form.name.data, role=user_role)
+            user_role = Role.query.filter_by(name=form.role.data).first();
+            user = User(username=form.name.data, role=user_role);
             db.session.add(user)
             db.session.commit()
             session['known'] = False
         else:
             session['known'] = True
-
         session['name'] = form.name.data
         return redirect(url_for('index'))
-
-    # Recupera todos os usuários e conta a quantidade
-    user_all = User.query.all()
-    user_count = User.query.count()  # Conta os usuários diretamente no banco de dados
-    
-    # Recupera todas as funções e seus respectivos usuários
-    roles_all = Role.query.all()
-    role_count = Role.query.count()  # Conta as funções cadastradas
-
     return render_template('index.html', form=form, name=session.get('name'),
-                           known=session.get('known', False), user_all=user_all,
-                           user_count=user_count, roles_all=roles_all, 
-                           role_count=role_count)
+                           known=session.get('known', False),
+                           user_all=user_all, role_all = role_all);
