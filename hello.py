601f78589c20347930422b85bc8c8458724a2881
Ana Caroline <140763719+aanaca@users.noreply.github.com>
2024-08-30 21:48:00 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -25,6 +25,25 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
 class Disciplina(db.Model):
     __tablename__ = 'disciplinas'
     id = db.Column(db.Integer, primary_key=True)
